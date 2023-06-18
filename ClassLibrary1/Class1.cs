c3e2f493b8ee6ae4984e40f471f15c93ec02df64
omranali2111 <aljabri1234@hotmail.com>
2023-06-18 11:22:20 +0400

    static

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        int numberofweel;
+        public static int somenumber = 2;
+
+        public Class1(int parameter)
+        {
+            numberofweel = parameter;
+        }
+
+        public int getNumberofWeels() {
+
+            somenumber = somenumber * 10;
+            return numberofweel*2;
+        
+        
+        }
+
+        public static void getNumberofstaticweels()
+
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+}
---
+++ c-sharp-Access-Modifiers/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary1;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Class1 anynumber = new Class1(5);
+        Console.WriteLine(Class1.somenumber);
+        Console.WriteLine(anynumber.getNumberofWeels());
+
+        Console.WriteLine("&&&&&&&&&&&&&&&");
+
+        Class1 anyvalue = new Class1(6);
+        Console.WriteLine(Class1.somenumber);
+        anyvalue.getNumberofWeels();
+
+        Console.WriteLine(anyvalue.getNumberofWeels());
+
+        Class1 anyvalue2 = new Class1(7);
+        Console.WriteLine(Class1.somenumber);
+
+
+
+
+    }
+}
